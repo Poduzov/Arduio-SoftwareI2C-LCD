5bd56e4b9574fd72429eb490a60ffa1d140e4906
Denis Poduzov <poduzov@gmail.com>
2019-01-05 20:01:31 -0500

    Working test

--- Arduio-SoftwareI2C-LCD.ino
+++ Arduio-SoftwareI2C-LCD.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "LCDSoftI2C.h"
 
+LCDSoftI2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, world!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
